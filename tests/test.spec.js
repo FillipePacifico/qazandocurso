d4649131b8936c231e4e06598b288d55c31006eb
Fillipe Pacifico <fillipe.pacifico@gmail.com>
2024-05-14 17:26:51 -0300

    Automação Playwright

---
+++ tests/test.spec.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@playwright/test');
+
+test('Teste de Login @login' , async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('teste@teste.com.br');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
