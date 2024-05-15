3be7509ff473f0568e185b83644d1bf1ed52c007
Fillipe Pacifico <fillipe.pacifico@gmail.com>
2024-05-15 12:29:53 -0300

    Alteração nova branch

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,12 +1,15 @@
 const { test, expect } = require('@playwright/test');
 
-test('Teste de Login @login' , async ({ page }) => {
+test('Testando0205 de Login @login' , async ({ page }) => {
   await page.goto('https://automationpratice.com.br/');
   await page.getByRole('link', { name: ' Login' }).click();
   await page.locator('#user').click();
   await page.locator('#user').fill('teste@teste.com.br');
-  await page.locator('#password').click();
+  await page.locator('#password').clicSk();
   await page.locator('#password').fill('123456');
   await page.getByRole('button', { name: 'login' }).click();
-  await page.getByRole('button', { name: 'OK' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();SS
+
+
+  //mudanca para master
 });
